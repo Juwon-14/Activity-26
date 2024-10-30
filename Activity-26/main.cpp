d69e8b71d9243a206b71ecf9047ccc36889c65b1
Juwon-14 <wndnjs9127@gmail.com>
2024-10-30 11:31:26 -0700

    Update main.cpp

--- Activity-26/main.cpp
+++ Activity-26/main.cpp
@@ -2,13 +2,26 @@
 //  main.cpp
 //  Activity-26
 //
-//  Created by 홍주원 on 10/29/24.
+//  Created by Juwon Hong on 10/29/24.
 //
 
 #include <iostream>
+#include "Point.h"
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    Point p1 = {7, 9};
+    Point p2 = {7, 9};
+    Point p3 = {10, 15};
+
+    if (p1 == p2) {
+        cout << "p1 is equal to p2" << endl;
+    } else {
+        cout << "p1 is not equal to p2" << endl;
+    }
+
+    cout << "Point p1: " << p1 << endl;
+    cout << "Point p3: " << p3 << endl;
+
     return 0;
 }
