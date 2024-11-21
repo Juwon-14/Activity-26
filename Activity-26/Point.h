a96459bcc16dd90ea927f365ee7c6438ec913d50
Juwon-14 <wndnjs9127@gmail.com>
2024-11-21 11:04:14 -0800

    Update Point.h

--- Activity-26/Point.h
+++ Activity-26/Point.h
@@ -9,17 +9,15 @@
 #define POINT_H
 
 #include <iostream>
-using namespace std;
 
 struct Point {
     int x;
     int y;
 
-    
     bool operator==(const Point& other) const;
 };
 
-
-ostream& operator<<(ostream& output, const Point& point);
+std::ostream& operator<<(std::ostream& output, const Point& point);
 
 #endif
+
